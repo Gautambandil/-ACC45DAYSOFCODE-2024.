ea6deeb1886d5e10dcbf983d8f5e63817857d34b
Gautambandil <gautambandil03@gmail.com>
2024-11-12 20:12:06 +0530

    Add files via upload

---
+++ Day-37 BATH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        int waterRequiredPerPerson = 2 * Y;
+        int maxPeople = X / waterRequiredPerPerson;
+        cout << maxPeople << endl;
+    }
+    return 0;
+}
